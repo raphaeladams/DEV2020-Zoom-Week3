4291a24c3c4100f2f1ef488748190324be9f889d
Raphael Adams <raphael.adams@shopify.com>
2020-10-20 13:19:15 -0400

    Use .map to make adjectives array, capitalize/put in quotes

--- reviews.rb
+++ reviews.rb
@@ -1,25 +1,24 @@
 # Code Along: Reviews
 # Raphael Adams
 
-lines = []
-
-review_file = File.open("reviews.txt") do |review_file|
-    lines = review_file.readlines 
-end
-
-relevant_lines = lines.find_all { |line| line.include?("Truncated") }
-reviews = relevant_lines.reject { |line| line.include?("--") }
-
 def find_adjective(string)
     words = string.split(" ")
     index = words.find_index("is")
     words[index + 1] 
 end
 
-adjectives = []
-
-reviews.each do |review|
-    adjectives << find_adjective(review)
+lines = []
+File.open("reviews.txt") do |review_file|
+    lines = review_file.readlines 
 end
 
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
+
+adjectives = reviews.map do |review| 
+    adjective = find_adjective(review)
+    "'#{ adjective.capitalize }'"
+end
+
+puts "Critics agree, Truncated is:"
 puts adjectives
