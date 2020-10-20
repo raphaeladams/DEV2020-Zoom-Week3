6a9f9e2e7fc7872b0906a220d775a7c6e6f0b692
Raphael Adams <raphael.adams@shopify.com>
2020-10-20 17:46:57 -0400

    Add loop to calculate change owed

--- change.rb
+++ change.rb
@@ -1,3 +1,14 @@
 # Cashmoney
 # Raphael Adams
 
+change_owed = -1
+
+loop do
+  print "How much change is owed? "
+  input = gets.chomp
+  next unless input.match?(/^\d*\.\d*$/)
+  total_cents = (input.to_f * 100).round
+  change_owed = total_cents % 5 > 2 ? (total_cents/5 + 1) * 5 : (total_cents/5) * 5
+  puts change_owed
+  break
+end
