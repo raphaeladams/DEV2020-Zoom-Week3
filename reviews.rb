3701bc6d4ab8e47da57cb6ec07d3db94ae353c2c
Raphael Adams <raphael.adams@shopify.com>
2020-10-20 11:09:15 -0400

    Add code to find relevant lines

--- reviews.rb
+++ reviews.rb
@@ -7,4 +7,12 @@ review_file = File.open("reviews.txt") do |review_file|
     lines = review_file.readlines 
 end
 
-puts lines.length
+relevant_lines = []
+
+lines.each do |line|
+    if line.include?("Truncated")
+        relevant_lines << line
+    end
+end
+
+puts relevant_lines
