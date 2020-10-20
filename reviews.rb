d3f562f1a7604c22206968dfa783968a3442da03
Raphael Adams <raphael.adams@shopify.com>
2020-10-20 11:11:12 -0400

    Replace .each with find_all

--- reviews.rb
+++ reviews.rb
@@ -7,12 +7,6 @@ review_file = File.open("reviews.txt") do |review_file|
     lines = review_file.readlines 
 end
 
-relevant_lines = []
-
-lines.each do |line|
-    if line.include?("Truncated")
-        relevant_lines << line
-    end
-end
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 
 puts relevant_lines
