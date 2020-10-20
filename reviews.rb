e51f2019f6dd24e6ec79ac0109bc838435357584
Raphael Adams <raphael.adams@shopify.com>
2020-10-20 11:53:18 -0400

    Break string into array of adjectives

--- reviews.rb
+++ reviews.rb
@@ -10,4 +10,16 @@ end
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 reviews = relevant_lines.reject { |line| line.include?("--") }
 
-puts reviews
+def find_adjective(string)
+    words = string.split(" ")
+    index = words.find_index("is")
+    words[index + 1] 
+end
+
+adjectives = []
+
+reviews.each do |review|
+    adjectives << find_adjective(review)
+end
+
+puts adjectives
