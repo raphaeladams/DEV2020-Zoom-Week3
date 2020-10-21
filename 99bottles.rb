eacb97c01e1a44696aa62cdecfe6991cb6d68e88
Raphael Adams <raphael.adams@shopify.com>
2020-10-21 12:08:58 -0400

    Output last 2 verses

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,7 +1,29 @@
 # 99 Bottles
 # Raphael Adams
 
-puts "No more bottles of beer on the wall,"
-puts "no more bottles of beer."
-puts "Go to the store and buy some more,"
-puts "99 bottles of beer on the wall."
+def verse(number)
+  case number
+  when 1
+    puts "#{ number } bottle of beer on the wall,"
+    puts "#{ number } bottle of beer."
+    puts "Take one down and pass it around,"
+    puts "no more bottles of beer on the wall."
+    puts "\n"
+  when 0
+    number = "no more"
+    puts "#{ number.capitalize } bottles of beer on the wall,"
+    puts "#{ number } bottles of beer."
+    puts "Go to the store and buy some more,"
+    puts "99 bottles of beer on the wall."
+    puts "\n"
+  end
+end
+
+def song(number)
+  until number < 0
+    verse(number)
+    number -= 1
+  end
+end
+
+song(1)
