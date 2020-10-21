7ee85685344070b0c3b2926da278b368e36602a2
Raphael Adams <raphael.adams@shopify.com>
2020-10-21 10:56:05 -0400

    Added last verse

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,2 +1,7 @@
 # 99 Bottles
 # Raphael Adams
+
+puts "No more bottles of beer on the wall,"
+puts "no more bottles of beer."
+puts "Go to the store and buy some more,"
+puts "99 bottles of beer on the wall."
