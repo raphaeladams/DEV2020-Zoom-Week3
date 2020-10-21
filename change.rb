92e47d3ff1698f87f99917b68d63add5370ace85
Raphael Adams <raphael.adams@shopify.com>
2020-10-20 21:47:04 -0400

    Add method & block to count coins, storing in hash

--- change.rb
+++ change.rb
@@ -1,14 +1,34 @@
 # Cashmoney
 # Raphael Adams
 
-change_owed = -1
+
+def count_coins(change, all_coins, total_coins)
+  yield("toonies", 200)
+  yield("loonies", 100)
+  yield("quarters", 25)
+  yield("dimes", 10)
+  yield("nickels", 5)
+end
+
+
+change = -1
+total_coins = 0
+all_coins = Hash.new
 
 loop do
   print "How much change is owed? "
   input = gets.chomp
   next unless input.match?(/^\d*\.\d*$/)
-  total_cents = (input.to_f * 100).round
-  change_owed = total_cents % 5 > 2 ? (total_cents/5 + 1) * 5 : (total_cents/5) * 5
-  puts change_owed
+  change = (input.to_f * 100).round
   break
 end
+
+count_coins(change, all_coins, total_coins) do |coin_name, coin_worth|
+  num_coins = change / coin_worth
+  change = change % coin_worth
+  all_coins[coin_name] = num_coins
+  total_coins += num_coins
+end
+
+puts all_coins
+puts total_coins
